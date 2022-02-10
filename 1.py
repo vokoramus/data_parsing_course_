345e0254db71874542e3fba8751cbddcb6de8475
Yuriy <vokoramus01@ya.ru>
2022-02-10 19:36:52 +0300

    Initial commit

---
+++ 1.py
@@ -0,0 +1,11 @@
+import requests
+import json
+appid = 'bbcf00e8fd39b3bfdedace784dd36893'
+service = 'https://samples.openweathermap.org/data/2.5/weather'
+req = requests.get(f'{service}?q=London,uk&appid={appid}')
+data = json.loads(req.text)
+print(f"В городе {data['name']} {data['main']['temp']} градусов по Кельвину")
+
+print()
+
+https://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=bbcf00e8fd39b3bfdedace784dd36893
